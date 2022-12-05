01cb613dad32eb8c7d95d2552fb2472a00699d4d
Arpitttt2310 <100558502+Arpitttt2310@users.noreply.github.com>
2022-12-06 00:18:12 +0530

    Create cube3.py
    program to calculate cube of a number using Exponent Operator

---
+++ cube3.py
@@ -0,0 +1,10 @@
+# Python program to calculate cube of a number using Exponent Operator
+ 
+# take input from user
+num = int (input("Enter an any number: "))
+ 
+# calculate cube using Exponent Operator
+cb = num**3
+ 
+# print
+print("Cube of {0} is {1} ".format(num, cb))
